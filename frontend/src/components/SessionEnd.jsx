ca3c34009c1ebdf72ba2a9915acd34e0c6219437
Evil-Shown <damitharaveesh@gmail.com>
2026-04-17 20:20:24 +0530

    documentation Updated

--- frontend/src/components/SessionEnd.jsx
+++ frontend/src/components/SessionEnd.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SessionEnd.css";
+import "../styles/SessionEnd.css";
 
 function SessionEnd({ onClose }) {
   const navigate = useNavigate();
